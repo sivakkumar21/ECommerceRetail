16c53450919866ee50aa4fbaec3893a06804b5af
SIVAKKUMAR PALANISAMY <sivakkmr21@gmail.com>
2024-05-28 15:22:03 +0530

    products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,45 @@
-import "./App.css";
+import { Col, Container, Nav, Navbar, NavbarBrand, Row } from "react-bootstrap";
+import { sampleProducts } from "./data";
 
 function App() {
   return (
-    <div>
-      <header>TS Amazona</header>
-      <main></main>
-      <footer>All rights reserverd</footer>
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <NavbarBrand>TS Amazonas</NavbarBrand>
+          </Container>
+          <Nav>
+            <a href="/cart" className=" nav-link">
+              Cart
+            </a>
+            <a href="/sign-in" className="  nav-link">
+              Sign In
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
+      <main>
+        <Container>
+          <Row>
+            {sampleProducts.map((item) => (
+              <Col key={item.slug} sm={6} md={4} lg={3}>
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className="product-image"
+                />
+                <h3>{item.name}</h3>
+                <p>{item.price}</p>
+                <p>{item.description}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </main>
+      <footer>
+        <div className="text-center">All rights reserverd</div>
+      </footer>
     </div>
   );
 }
